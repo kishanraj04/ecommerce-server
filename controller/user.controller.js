3e8e9f9ddb6da185582e3d912cc327f8ca5102ba
kishanraj04 <kishuraj1111@gmail.com>
2025-02-03 23:55:36 +0530

    creating api for storing user

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -3,8 +3,15 @@ import userModel from '../model/userModel.js'
 
 
 // register user
-export const signUpUser = asyncHandler((req,res)=>{
-       res.end('up')
+export const signUpUser = asyncHandler(async(req,res)=>{
+    
+    const createdUser = await userModel.create(req?.body)
+    
+    res.status(200).json({
+        sucess:true,
+        message:"user created sucess",
+        createdUser
+    })
 })
 
 
