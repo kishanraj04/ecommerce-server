a25a3f80807fc2b8f5391c93968c19eb9c4c77b5
kishanraj04 <kishuraj1111@gmail.com>
2025-02-06 12:17:36 +0530

    creating model for order

---
+++ model/order.model.js
@@ -0,0 +1,116 @@
+import mongoose from "mongoose";
+
+const orderSchema = mongoose.Schema({
+  shippingInfo: {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    contact: {
+      type: String,
+      required: true,
+    },
+    alternatePhone: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    region: {
+      type: String,
+      required: true,
+    },
+    state: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    landMark: {
+      type: String,
+    },
+  },
+  orderItems:[
+    {
+     name:{
+        type:String,
+        required:true
+     },
+     price:{
+        type:Number,
+        required:true
+     },
+     qty:{
+        type:Number,
+        required:true
+     },
+     product:{
+        type:mongoose.Schema.ObjectId,
+        ref:"Product",
+        required:true
+     }
+    }
+  ],
+  user:{
+    type:mongoose.Schema.ObjectId,
+    ref:"usermodel",
+    required:true
+  },
+  paymentInfo:{
+    paymentId:{
+        type:String,
+        required:true
+    },
+    paymentStatus:{
+        type:String,
+        required:true
+    }
+  },
+  paidAt:{
+    type:Data,
+    required:true
+  },
+  itemPrice:{
+    type:Number,
+    required:true,
+    default:0
+  },
+  tax:{
+    type:Number,
+    required:true,
+    default:0
+  },
+  shippingPrice:{
+    type:Number,
+    required:true,
+    default:0
+  },
+  totalPrice:{
+    type:Number,
+    required:true,
+    default:0
+  },
+  orderStatus:{
+    type:String,
+    required:true,
+    default:"processing"
+  },
+  deliveredAt:{
+    type:Data
+  },
+  createdAt:{
+    type:Data,
+    default:Data.now
+  }
+
+});
+
+export default mongoose.model('orderModel',orderSchema)
