d98c35bff33c8134353af385abd1c4a9bc34ef06
kishanraj04 <kishuraj1111@gmail.com>
2025-02-07 11:17:10 +0530

    some change in getting single order

--- controller/order.controller.js
+++ controller/order.controller.js
@@ -1,9 +1,12 @@
 import { asyncHandler } from "../middleware/asyncErrorHandler.js";
 import orderModel from '../model/order.model.js'
+import userModel from "../model/userModel.js";
 
 // creating order
 export const orderTheProduct = asyncHandler(async (req,res)=>{
-    const data = req?.body 
+    const loginUser = req?.user
+    console.log(loginUser);
+    const data = {...(req?.body ),user:loginUser?._id}
     const addedOrder = await orderModel.create(data)
 
     res.status(200).json({success:true , message:"product order successfull",addedOrder})
@@ -25,7 +28,9 @@ export const getAllOrder = asyncHandler(async (req,res) => {
 // get single order
 export const getSingleOrder = asyncHandler(async (req,res) => {
     const {id} = req?.params
-    const singleOrder = await orderModel.findOne({_id:id})
+    const singleOrder = await orderModel.findOne({_id:id}).populate('user',"name email")
+    console.log("so ",singleOrder);
+   
     if(!singleOrder){
         return res.status(404).json({success:false , message:"order not found"})
     }else{
