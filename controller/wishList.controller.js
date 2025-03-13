57fb0779e689fa80feead78ad066263b5d09f1a9
kishanraj04 <kishuraj1111@gmail.com>
2025-03-13 21:29:09 +0530

    creating the route and controller for deleteing the wishlist item

--- controller/wishList.controller.js
+++ controller/wishList.controller.js
@@ -3,6 +3,7 @@ import { wishListModel } from "../model/wishlist.model.js";
 
 // add to wishList
 export const addToWishList = asyncHandler(async(req,res)=>{
+    console.log("run A");
     const {_id} = req?.user
     const {pid,price} = req?.body
     console.log(_id,pid,price);
@@ -12,11 +13,26 @@ export const addToWishList = asyncHandler(async(req,res)=>{
 
 })
 
+// get all wishList
 export const getAllWishListData = asyncHandler(async(req,res)=>{
+    console.log("run B");
     const {_id} = req?.user 
     const wishListData = await wishListModel.find({userId:_id})
     if(!wishListData){
         return res.status(404).json({success:false,message:"user not found"})
     }
     res.status(200).json({success:true,message:"data found",wishListData})
+})
+
+// remove wishList
+export const deleteFromWishList = asyncHandler(async(req,res)=>{
+    const {id} = req?.params 
+    const deleteResp = await wishListModel.deleteOne({_id:id})
+    
+    if(!deleteResp){
+    return  res.status(409).json({success:false,message:"product is not deleted"})
+    }
+
+    res.status(200).json({success:true,message:"product deleted",deleteResp})
+
 })
--- routes/wishList.Route.js
+++ routes/wishList.Route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { isAuthenticated } from '../middleware/isAuthenticated.js'
-import { addToWishList, getAllWishListData } from '../controller/wishList.controller.js'
+import { addToWishList, deleteFromWishList, getAllWishListData } from '../controller/wishList.controller.js'
 
 export const wishListRouter = express.Router()
 
@@ -9,3 +9,6 @@ wishListRouter.post('/wish-list/add',isAuthenticated,addToWishList)
 
 // get all wishlist item
 wishListRouter.get('/wish-list/data',isAuthenticated,getAllWishListData)
+
+// remove from wishlist
+wishListRouter.delete('/wish-list/delete/:id',isAuthenticated,deleteFromWishList)
